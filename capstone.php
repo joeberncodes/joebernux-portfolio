a6bdeab1748d515e2094ea9b93fb4937def5e1d7
Joe Bernstein <joebernux@gmail.com>
2018-04-17 01:12:17 -0700

    Add files via upload

---
+++ capstone.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="ie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 10)|!(IE)]><!-->
+<html lang="en">
+<!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs ================================================== -->
+	<meta charset="utf-8">
+	<title>Capstone | JoeBernUX</title>
+	<meta name="description" content="Joe Bernstein works to continue learning inside and outside of the classroom to further improve his UX design skills.">
+	<meta name="author" content="JoeBernUX">
+	<!-- Mobile Specific Metas ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- CSS ================================================== -->
+	<!-- web font  -->
+	<link href='https://fonts.googleapis.com/css?family=Libre+Baskerville:400,700' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+	<!-- Neko framework  -->
+	<link type="text/css" rel="stylesheet" href="custom-icons/css/custom-icons.css">
+	<link type="text/css" rel="stylesheet" href="neko-framework/external-plugins/external-plugins.min.css">
+	<link type="text/css" rel="stylesheet" href="neko-framework/css/layout/neko-framework-layout.css">
+	<link type="text/css" rel="stylesheet" id="color" href="neko-framework/css/color/neko-framework-color.css">
+	<!--added CDN link from Lynda-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<!-- Favicons ================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="images/apple-touch-icon-144x144.png">
+	<!-- <link rel="stylesheet" href="http://basehold.it/30"> -->
+
+	<script src="neko-framework/external-plugins/modernizr/modernizr.custom.js"></script>
+
+	<!-- Owl CSS -->
+	<link type="text/css" rel="stylesheet" href="owl-carousel-2/assets/owl.carousel.min.css">
+	<link type="text/css" rel="stylesheet" href="owl-carousel-2/assets/owl.theme.default.min.css">
+
+
+</head>
+<body class="activate-appear-animation header-dark nav-style-1">
+
+	<div class="little-neko-preloader little-neko-sk-cube-grid little-neko-preloader-centered" data-logo="images/JBLogo-32x32.png"></div>
+
+	<!-- global-wrapper -->
+	<div id="global-wrapper">
+		<!-- header -->
+
+		<?php include('header.php');?> 
+
+
+		<!-- header -->
+		<!-- ======================================= content ======================================= -->
+
+		<header class="page-header x-large image-background image-12 dark-color center no-padding parallax" data-type="background" data-speed="10">
+			<div class="mask opacity-5">
+
+				<div class="container v-align-center">
+					<div class="row">
+						<div class="col-md-12 text-light">
+							<h1 class="x-large block no-mb">Flying high to the finish line and flying far away to celebrate.</h1>
+							<h2>
+								Drifter: A Last-minute Travel Solution<br>
+                                Role: Project Manager, Prototyper, Video Editor
+							</h2>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</header>
+
+		<main id="content">
+			<div class="pt-large pb-large">
+
+				<div class="container">
+					<div class="row">
+
+						<article class="post-content ">
+							<div class="col-md-8 col-md-offset-2">
+								<p class="lead">
+								Expedia has cornered many angles of the travel market, but while they have added many features in recent years to accommodate the highly organized vacation planner, one user segment missing from their domain was the last-minute wanderlust traveler. Who are these people who fill your instagram feed with travel dreams and seem to hop aboard a plane on a whim? Expedia asked us to investigate this user segment as the sponsor for our HCDE capstone project. <a href="process_book.pdf">See the full process book here.</a>
+							</p>
+								
+                                <h3>The problem:</h3> <br>
+                                    <p>
+                                    When booking a trip within a week of departure, the last-minute traveler doesn’t feel that there is a booking experience that offers the personalized, trustworthy, and flexible process they desire.
+
+									</p>
+                                    <div class="post-pic col-md-12 col-md-offset-0">
+								<img src="images/project-images/Design_Process.png" alt="" class="responsive" />
+								<p class="small mb mt-small">A visual representation of our design process.</p>
+							</div>
+                                    
+									<p>
+									We scoured 19 different popular travel booking websites like Hotwire, Priceline, and Google Flights. We surveyed 69 frequent flyers and then followed up with a more in-depth survey that reached 113 respondents. Additionally, we interviewed 8 of those respondents to drill into the facts behind their survey answers. These led us to the following research conclusions: 
+                                        <ol>
+                                        <li>
+                                            Travelers can be overwhelmed by the wide selection of travel choices available.
+</li>
+                                <li>
+                                            Travelers believe that travel websites are manipulative in how they present booking information to users.
+</li>
+                                <li>
+                                            Travelers want a travel booking experience that does not feel like a huge commitment of time and money.
+</li></ol>
+								</p>
+                            
+                            <p>We translated these research conclusions into three overarching design goals: Trust, Curation, and Flexibility.</p>
+                            
+                            
+                            <div class="post-pic col-md-12 col-md-offset-0">
+								<img src="images/project-images/Design_Goals.png" alt="" class="responsive" />
+								<p class="small mb mt-small">Our three most important goals to target.</p>
+							</div>
+
+							</div>
+				
+
+
+							<div class="col-md-8 col-md-offset-2 mb-medium">
+									
+								<h3>Trust us; we did the research</h3>
+								<p>
+									Taking those three design goals and running with them, we embarked on our prototyping process. This involved several whiteboarding sessions to deliver an Information Architecture flowchart and subsequent paper prototype screens to get quick test results from potential users. Some early things this helped us identify was our wording of certain tasks, like Favorites, Search vs. Browse, and the Sell-back feature that was meant to instill trust in our users.  
+								</p>
+							</div>
+							<div class="post-pic col-md-4 col-md-offset-4">
+								<img src="images/project-images/Capstone_IA.png" alt="" class="responsive" width="570"/>
+								<p class="small mb mt-small">A basic diagram of the IA we settled on.</p>
+							</div>
+							<div class="col-md-8 col-md-offset-2">	
+								<h3>Quick! Test it on a screen!</h3>
+								<p>
+								    After cleaning up our content strategy, we shifted our focus to the flow. As we wanted to design a travel shopping experience centered around quick gestures and swiping, building out an interactive experience was paramount. My teammate Val built a series of linked wireframes using Sketch to test users' ability to interact with the product. 
+								</p>
+							</div>
+
+							<div class="col-md-8 col-md-offset-2">	
+								<h3>Preparing for a smooth landing</h3>
+								<p>
+									The final phase of the project involved incorporating all of our user feedback from the wireframes into high fidelity screens. 
+                                    
+                                    What we changed: <br>
+                                    <ul>
+                                    <li><b>Swiping: </b>One of our key hypotheses in the deisgn process was to incorporate the swiping interactions of dating apps like Tinder to help the decision-making process. After all, finding a good vacation should feel like finding a good partner. Our user tests disagreed, possibly due to the fact that our prototypes couldn't mimic those gestures. Instead, our users indicated that they preferred to scroll through choices and see multiple options at a time for comparison sake.
+                                        <img src="images/project-images/OfferPage.png" alt="" class="responsive" width="570"/></li>
+                                    <li><b>Barriers to entry: </b>We deliberated for a while on whether or not we should include a sign-up button on the welcome screen. This is a platform that collects data,  personalizes interests, and includes financial transactions. Creating an account early seemed like a good idea. Our early tests hinted that users were unconvinced in the value of such an app and probably wouldn't sign up until they reap some of its benefits. We resolved to use active verbs on the welcome page and skip right to the good stuff. The user can sign up once they want to start customizing their experience.
+                                        <img src="images/project-images/WelcomePage.png" alt="" class="responsive" width="570"/></li>
+                                    <li><b>Helper text: </b>This one is pretty straightforward. Our goal was to create a new experience for finding travel plans. We wanted to be different from the existing apps out there. So it's not a stretch to believe that users could benefit from some onboarding and tutorial experience. Even though it conflicts with our effort to remove barriers to entry, we included features like Bucket List and Sell-back whose uses aren't immediately intuitive. By requiring a brief, friendly onboarding experience, we welcome our users to test out the keys that set us apart from other apps. <img src="images/project-images/Onboarding.png" alt="" class="responsive" width="570"/></li>
+                                    </ul>
+								</p>
+							</div>
+
+							<div class="post-pic col-md-8 col-md-offset-2">
+								<img src="images/project-images/HiFiPrototypes.png" alt="The team's final wireframes" class="responsive"/>
+								<p class="small mb mt-small">Our high-fidelity prototypes, designed in Sketch by our teammate Val.</p>
+							</div>
+                        
+                        
+                        <div class="col-md-8 col-md-offset-2">	
+								<h3>Pitching the Concept</h3>
+								<p>
+									As part of our final deliverables, we created a video to convey the purpose, problem, and solution of Drifter. This was a task I undertook with the help of my friends as actors. I storyboarded the filming process and edited the pieces together. 
+								</p>
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/IBWCb7W96Wo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> <p><a href="https://youtu.be/IBWCb7W96Wo">Drifter: A Last-Minute Travel Solution</a> 
+							</div>
+
+							<div class="col-md-8 col-md-offset-2">	
+								<h3>Elation and Celebration</h3>
+								<p>
+									 I couldn't spend my last quarter of grad school staring at travel deals without taking advantage of my new expertise. After tracking several websites over the course of the project, I found a great deal on a flight to Thailand and spent two weeks wanderlusting around the country, never booking plans more than two days in advance.  
+								</p>
+								<img src="images/Krabi.jpg" alt="Tiger Temple, Krabi" class="responsive"/>
+								<p class="small mb mt-small">Smiling from the Tiger Temple in Krabi, Thailand.</p>
+								
+                            
+                            
+
+						</article>
+
+					</div><!-- row -->
+				</div><!-- container -->
+			</div>
+
+		</main>
+		<!-- content -->
+		
+		<!-- footer -->
+		
+        <?php include('footer.php');?> 
+        
+		<!-- / footer -->		
+	</div>
+	<!-- global wrapper -->
+
+	<!-- End Document ================================================== -->
+
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script type="text/javascript" src="neko-framework/js/jquery/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="neko-framework/js/jquery-ui/jquery-ui-1.8.23.custom.min.js"></script>
+
+	<!-- external framework plugins -->
+	<script type="application/javascript" src="neko-framework/external-plugins/external-plugins.min.js"></script>
+	
+	<!-- neko framework script -->
+	<script type="text/javascript" src="neko-framework/js/neko-framework.js"></script>
+
+	<!-- Owl -->
+	<script type="text/javascript" src="owl-carousel-2/owl.carousel.min.js"></script>
+
+	<!-- neko custom script -->	
+	<script src="js/custom.js"></script>
+    
+     <!-- my script -->	
+	<script src="js/script.js"></script>
+</body>
+</html>
